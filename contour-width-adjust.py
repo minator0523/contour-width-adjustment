6d01d2cabe57f98f32b4a1ef363bb40169db3b3d
minator0523 <procyon523@gmail.com>
2025-03-06 20:59:02 +0900

    もとのデータをGSMから再解析NCEP2に変更

--- contour-width-adjust.py
+++ contour-width-adjust.py
@@ -31,16 +31,15 @@ lat_center = 35.0 # 中心緯度
 lon_center = 135.0 # 中心経度
 
 ## GSMデータ（gribをNetCDFに変換したもの）を読み込む
-ifile = f'./data/GSM_jp-surf_{yyyy:04d}{mm:02d}{dd:02d}.nc'
+ifile = f'./data/mslp.2024.nc'
 ds_surf = xr.open_dataset(ifile)
 print(f"ifile: {ifile}")
-slp = ds_surf['slp'].sel(time=f"{yyyy:04d}-{mm:02d}-{dd:02d}T{hh:02d}:00").values # 海面校正気圧
-lats = ds_surf['latitude'].values # 緯度
-lons = ds_surf['longitude'].values # 経度
+slp = ds_surf['mslp'].sel(time=f"{yyyy:04d}-{mm:02d}-{dd:02d}T{hh:02d}:00").values / 100.0 # 海面校正気圧
+lats = ds_surf['lat'].values # 緯度
+lons = ds_surf['lon'].values # 経度
 
 # 作図に必要な配列を用意
 lines_value, linewidths, index_label = contour_lines(slp, 4.0, 1.5, 3.0)
-
 ## 作図
 x, y = np.meshgrid(lons, lats)
 fig, ax = plt.subplots(
@@ -49,10 +48,10 @@ fig, ax = plt.subplots(
 ax.text(0.0, 1.05, f'SLP, {yyyy:04d}/{mm:02d}/{dd:02d} {hh:02d}:00UTC ANL', transform=ax.transAxes, size=25, weight='bold') # タイトル
 # 地図を作成
 gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=1, linestyle=':', color='k', alpha=0.8) # 経度・緯度線を描写:
-gl.xlocator = mticker.FixedLocator(np.arange(120.0, 150.0, 10.0)) # 経度線の間隔を設定
-gl.ylocator = mticker.FixedLocator(np.arange(20.0, 50.0, 10.0)) # 緯度線の間隔設定
+gl.xlocator = mticker.FixedLocator(np.arange(120.0, 150.0, 20.0)) # 経度線の間隔を設定
+gl.ylocator = mticker.FixedLocator(np.arange(20.0, 50.0, 20.0)) # 緯度線の間隔設定
 ax.coastlines(color='black', linestyle='-', linewidth=0.75) # 海岸線を描画
-ax.set_extent([120.0, 150.0, 20.0, 50.0], ccrs.PlateCarree()) # 作図範囲の設定
+ax.set_extent([100.0, 160.0, 10.0, 50.0], ccrs.PlateCarree()) # 作図範囲の設定
 
 # 等値線を描画
 cs = ax.contour(x, y, slp, transform=ccrs.PlateCarree(), colors="green", levels=lines_value, linewidths=linewidths)
